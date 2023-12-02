06e945d86fbd70407ae0b154ed1f1f5301750eac
AlpiGun5 <a.i.guncan02@gmail.com>
2023-12-02 07:20:32 +0100

    Misc optimizations to day 2

--- src/solutions/day2.rs
+++ src/solutions/day2.rs
@@ -5,31 +5,31 @@ const SET: &[(&str, usize)] = &[("red", 12), ("green", 13), ("blue", 14)];
 
 pub fn solve(input: &str) -> (String, String, Duration) {
     let t = Instant::now();
-    let r = Regex::new(r"(\d+) (\w+),?").unwrap();
+    let r = Regex::new(r"\d+ \w+").unwrap();
     let p1 = input
         .lines()
         .enumerate()
         .filter(|l| {
-            l.1.split(';').all(|p| {
-                r.captures_iter(p).all(|m| {
-                    m.get(1).unwrap().as_str().parse::<usize>().unwrap()
-                        <= SET
-                            .iter()
-                            .find_map(|v| (v.0 == m.get(2).unwrap().as_str()).then_some(v.1))
-                            .unwrap_or(100)
-                })
+            r.find_iter(l.1).all(|m| {
+                let vv = m.as_str().split(' ').collect::<Vec<&str>>();
+                vv[0].parse::<usize>().unwrap()
+                    <= SET
+                        .iter()
+                        .find_map(|v| (v.0 == vv[1]).then_some(v.1))
+                        .unwrap_or(100)
             })
         })
         .fold(0, |acc, e| acc + e.0 + 1);
     let p2 = input
         .lines()
         .map(|l| {
-            r.captures_iter(l).fold((0, 0, 0), |mut acc, e| {
-                let amount = e.get(1).unwrap().as_str().parse::<usize>().unwrap();
+            r.find_iter(l).fold((0, 0, 0), |mut acc, e| {
+                let vv = e.as_str().split(' ').collect::<Vec<&str>>();
+                let amount = vv[0].parse::<usize>().unwrap();
                 match SET
                     .iter()
                     .enumerate()
-                    .find_map(|v| (v.1 .0 == e.get(2).unwrap().as_str()).then_some(v.0))
+                    .find_map(|v| (v.1 .0 == vv[1]).then_some(v.0))
                     .unwrap()
                 {
                     0 => acc.0 = usize::max(acc.0, amount),
