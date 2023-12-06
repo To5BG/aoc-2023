05ba96734a8b21077dce2990eb42ef4419a88617
AlpiGun5 <a.i.guncan02@gmail.com>
2023-12-06 18:48:23 +0100

    Solve day 6

--- src/solutions.rs
+++ src/solutions.rs
@@ -6,6 +6,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 pub fn solve(day: &str, input: &str) -> (String, String, Duration) {
     match day {
@@ -15,6 +16,7 @@ pub fn solve(day: &str, input: &str) -> (String, String, Duration) {
         "4" => day4::solve(input),
         "5" => day5::solve(input),
         "6" => day6::solve(input),
+        "7" => day7::solve(input),
         _ => panic!("No such day solution implemented."),
     }
 }
--- src/solutions/day6.rs
+++ src/solutions/day6.rs
@@ -1,9 +1,37 @@
 use std::time::{Duration, Instant};
 
-pub fn solve(_input: &str) -> (String, String, Duration) {
+pub fn solve(input: &str) -> (String, String, Duration) {
     let t = Instant::now();
-    /* */
-    let p1 = String::from("");
-    let p2 = String::from("");
-    (p1, p2, t.elapsed())
+    let p1 = input
+        .split('\n')
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .filter_map(|n| n.parse::<u64>().ok())
+        .zip(
+            input
+                .split('\n')
+                .nth(1)
+                .unwrap()
+                .split_whitespace()
+                .filter_map(|n| n.parse::<u64>().ok()),
+        )
+        .map(|t| {
+            (1..t.0)
+                .find_map(|n| (n * (t.0 - n) > t.1).then_some(t.0 - 2 * n + 1))
+                .unwrap()
+        })
+        .reduce(|a, b| a * b);
+    let large = (
+        input.split('\n').next().unwrap().replace(' ', "")[5..]
+            .parse::<u64>()
+            .unwrap(),
+        input.split('\n').nth(1).unwrap().replace(' ', "")[9..]
+            .parse::<u64>()
+            .unwrap(),
+    );
+    let p2 = (1..large.0)
+        .find_map(|n| (n * (large.0 - n) > large.1).then_some(large.0 - 2 * n + 1))
+        .unwrap();
+    (p1.unwrap().to_string(), p2.to_string(), t.elapsed())
 }
