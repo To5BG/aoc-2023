5375511aa636beccf13f71861deee6d6c1b5296d
AlpiGun5 <a.i.guncan02@gmail.com>
2023-12-15 20:33:57 +0100

    Solve day 12
    pain

--- src/solutions.rs
+++ src/solutions.rs
@@ -12,6 +12,7 @@ mod day9;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 
 pub fn solve(day: &str, input: &str) -> (String, String, Duration) {
     match day {
@@ -27,6 +28,7 @@ pub fn solve(day: &str, input: &str) -> (String, String, Duration) {
         "10" => day10::solve(input),
         "11" => day11::solve(input),
         "12" => day12::solve(input),
+        "13" => day13::solve(input),
         _ => panic!("No such day solution implemented."),
     }
 }
--- src/solutions/day12.rs
+++ src/solutions/day12.rs
@@ -1,9 +1,67 @@
+use std::collections::HashMap;
 use std::time::{Duration, Instant};
 
-pub fn solve(_input: &str) -> (String, String, Duration) {
+pub fn solve(input: &str) -> (String, String, Duration) {
     let t = Instant::now();
-    /* */
-    let p1 = String::from("");
-    let p2 = String::from("");
-    (p1, p2, t.elapsed())
+    let mut map = HashMap::<usize, usize>::new();
+    let (p1, p2) = input
+        .lines()
+        .map(|l| {
+            let (_v, _r) = l.split_once(' ').unwrap();
+            let springs = _v.chars().collect::<Vec<char>>();
+            let steps = _r
+                .split(',')
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>();
+            map.clear();
+            let _p1 = dp(&mut map, &springs, &steps, None);
+            let large_springs = ("?".to_string() + _v).repeat(5)[1..]
+                .chars()
+                .collect::<Vec<char>>();
+            let large_steps = (",".to_string() + _r).repeat(5)[1..]
+                .split(',')
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>();
+            map.clear();
+            let _p2 = dp(&mut map, &large_springs, &large_steps, None);
+            (_p1, _p2)
+        })
+        .fold((0, 0), |acc, e| (acc.0 + e.0, acc.1 + e.1));
+    (p1.to_string(), p2.to_string(), t.elapsed())
+}
+
+fn dp(
+    mem: &mut HashMap<usize, usize>,
+    springs: &[char],
+    steps: &[usize],
+    ext: Option<usize>,
+) -> usize {
+    if steps.is_empty() && ext.is_some() {
+        return 0;
+    }
+    if springs.is_empty() {
+        return match (steps.len(), ext) {
+            (0, None) => 1,
+            (1, Some(x)) if x == steps[0] => 1,
+            _ => 0,
+        };
+    }
+    let k = 10000 * ext.unwrap_or(0) + 100 * springs.len() + steps.len();
+    if let Some(&prev) = mem.get(&k) {
+        return prev;
+    }
+    let res = match (springs.first().unwrap(), ext) {
+        ('.', None) => dp(mem, &springs[1..], steps, None),
+        ('.', Some(n)) if n == steps[0] => dp(mem, &springs[1..], &steps[1..], None),
+        ('.', Some(_)) => 0,
+        ('?', None) => dp(mem, &springs[1..], steps, Some(1)) + dp(mem, &springs[1..], steps, None),
+        ('?', Some(n)) if n == steps[0] => {
+            dp(mem, &springs[1..], &steps[1..], None) + dp(mem, &springs[1..], steps, Some(n + 1))
+        }
+        ('#', None) => dp(mem, &springs[1..], steps, Some(1)),
+        ('#' | '?', Some(n)) => dp(mem, &springs[1..], steps, Some(n + 1)),
+        _ => unreachable!(),
+    };
+    mem.insert(k, res);
+    res
 }
